53a2ad79c2dee959017c35e74c97b51a546b925a
amirhasanzadehpy <amirhasanzadehpy@gmail.com>
2024-08-22 15:46:54 +0330

    try to fix error github actions

--- app/config/settings.py
+++ app/config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
         "HOST": os.environ.get("DB_HOST"),
         "USER": os.environ.get("DB_USER"),
